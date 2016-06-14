3a1880293536ff28a31a7247b124309e19231eb6
Taylor D. Scott <tscott5@wisc.edu>
2016-06-14 14:25:10 -0500

    Add comments to run_simulation, condense k_act into a single value

--- KinaseODEs.m
+++ KinaseODEs.m
@@ -5,11 +5,11 @@ global n k_act k_deact k_on k_off k_cat
 dydt = zeros(3*n - 1, 1);
 
 if (n == 1)
-    dydt(1) = - k_act(1)*y(1) + k_deact(1)*y(2);
-    dydt(2) = + k_act(1)*y(1) - k_deact(1)*y(2);
+    dydt(1) = - k_act*y(1) + k_deact(1)*y(2);
+    dydt(2) = + k_act*y(1) - k_deact(1)*y(2);
 else
-    dydt(1) = - k_act(1)*y(1) + k_deact(1)*y(2);
-    dydt(2) = + k_act(1)*y(1) - k_deact(1)*y(2) - k_on(1)*y(2)*y(4) + k_off(1)*y(3) + k_cat(1)*y(3);
+    dydt(1) = - k_act*y(1) + k_deact(1)*y(2);
+    dydt(2) = + k_act*y(1) - k_deact(1)*y(2) - k_on(1)*y(2)*y(4) + k_off(1)*y(3) + k_cat(1)*y(3);
     dydt(3) = + k_on(1)*y(2)*y(4) - k_off(1)*y(3) - k_cat(1)*y(3);
     for i = 2:(n-1)
         dydt(3*i - 2) = -k_on(i-1)*y(3*i - 2)*y(3*(i-1)-1) + k_off(i-1)*y(3*(i-1)) + k_deact(i)*y(3*i - 1);
--- run_simulation.m
+++ run_simulation.m
@@ -1,12 +1,42 @@
 global n k_act k_deact k_on k_off k_cat
 
+% Description
+% This program generates a set of differential equations for an n-step
+% kinase cascade. The first kinase in the cascade is activated and
+% deactivated at a constant rate. The remaining kinases are activated and
+% deactivated according to a Michaelis-Menten like model. The function
+% models a total of 3*n - 1 components. The first two components are the
+% unactivated and activated forms of the first kinase respectively .The
+% remaining components are (for each kinase) (1) the deactivated form, (2)
+% the activated form and (3) the deactivated form complexed with the
+% activated form of the previous kinase. 
+% 
+% Usage
+% Specify n, k_act, k_deact, k_on, k_off, and k_cat and initial conditions. 
+% Then use one of MATLAB's built in ODE solvers to solve @KinaseODEs.
+
+% The number of kinases in the cascade
 n = 10;
-k_act = 0.5*ones(n, 1);
+
+% Rate constants for the equations
+
+% k_act is the activation rate of the inital kinase in the cascade
+k_act = 1;
+
+% k_deact is an array containing the deactivation rates of each kinase in
+% the cascade
 k_deact = 0.1*ones(n, 1);
+
+% k_on is the rate at which an activated kinase binds its substrate
 k_on = 0.5*ones(n, 1);
+
+% k_off is the rate at which an enzyme-substrate complex disassociates
 k_off = 0.5*ones(n, 1);
+
+% k_cat is the rate at which a kinase activates its substrate
 k_cat = 5*ones(n, 1);
 
+% Initial concentrations of each component of the cascade
 initials = [1 0 0 1 0 0 1 0 0 1 0 0 1 0 0 1 0 0 1 0 0 1 0 0 1 0 0 1 0];
 
 time_course = 0:0.01:30;
